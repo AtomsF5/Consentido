1c96e028bcc0c5491a31a13c5ae50821526d187c
J-ordiEs <70259721+J-ordiEs@users.noreply.github.com>
2021-01-25 15:31:28 +0100

    styles footer

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,10 +1,12 @@
-<footer>
+<div class="container mx-auto bg-indigo-900 text-white flex justify-around items-center">
     <div>Login</div>
     <div>Register</div>
-    <div>
-        <img src="{{asset()}}" alt="">
+    <div class="flex">
+        <div>
+            <img src="{{asset('images/insta.png')}}" alt="" class="">
+        </div>
+        <div>
+            <img src="{{asset('images/twitter.png')}}" alt="" class="">
+        </div>
     </div>
-    <div>
-        <img src="{{asset()}}" alt="">
-    </div>
-</footer>
+</div>
