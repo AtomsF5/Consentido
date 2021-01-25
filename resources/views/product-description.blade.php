0c2c636663da7751f0d24d0b579f2dc57d270b16
Mercedes <mvazquezsan@gmail.com>
2021-01-25 17:29:08 +0100

    added añadir al carrito button to product description blade

--- resources/views/product-description.blade.php
+++ resources/views/product-description.blade.php
@@ -1,46 +1,38 @@
 <!DOCTYPE html>
 <html lang="es">
-
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="{{ mix('css/app.css') }}">
-    <title>Página producto detallado</title>
-</head>    
-
-<header class="flex justify-center items-center">
-        <div>
-            <div >
-                <h1 class="text-2xl font-medium mt-7">DETALLE DE PRODUCTO</h1>
-            </div>
-            <div>
-                <img src="{{asset('images/LogoConSentido.png')}}" alt="Logo de ConSentido" class="w-32 h-24 ml-3 mt-3">
-            </div>
-        </div>
-</header>
-
-
+    <title>Descripción de producto</title>
+</head>
 <body>
-    <div>
-        <a href="#" class="bg-indigo-900 px-7 py-1 text-white rounded-sm">CARRITO</a>
-    </div> 
-    
+    <header>
+    </header>
+
+    <section class="container">
+        <div>
+            <button id="addCart">Añadir al carrito</button>
+            
+        </div>
+
+
+    </section>
 
     <div>
-        <img src="https://www.magnicenter.com/wp-content/uploads/2020/05/montana-gafas-de-lectura-720x491.jpg" alt="Gafas lectura plegable modelo F5">
+        <img src="" alt="Gafas lectura peglable modelo F5">
     </div>
 
-    <h3>Lista detallada de características del producto</h3>
+    <h3>Lista detallada características producto</h3>
 
-    <div role="list">
-        <div role="listitem"><span aria-label="1"></span> Tamaño de la lente 58 milímetros por 35 milímetros por 132 milímetros de ancho total</div>
-        <div role="listitem"><span aria-label="2"></span> Dispositivo de enganche recubierto en goma para evitar arañazos a sus gafas</div>
-        <div role="listitem"><span aria-label="3"></span> El sistema es plegable y viene acompañado de estuche</div>
-        <div role="listitem"><span aria-label="4"></span> Lente en material CR-39 orgánico</div>
-        <div role="listitem"><span aria-label="4"></span> Precio en Euros 25</div>
-    </div>
-
-
+<div role="list">
+    <div role="listitem"><span aria-label="1"></span> Tamaño de la lente – 58 mm x 35 mm x 132 mm Ancho total</div>
+    <div role="listitem"><span aria-label="2"></span> Dispositivo de enganche recubierto en goma para evitar arañazos a sus gafas.</div>
+    <div role="listitem"><span aria-label="3"></span> El sistema es abatible y de quita y pon rápido.</div>
+    <div role="listitem"><span aria-label="4"></span> Lente en material CR-39 orgánico.</div>
+</div>
+<div>
+    <p>Precio en Euros</p>
+</div>
     
 </body>
 </html>
