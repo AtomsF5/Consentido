87d41ca027c623efbf77e3f4f85fdaa968d44f1d
J-ordiEs <70259721+J-ordiEs@users.noreply.github.com>
2021-01-27 10:49:53 +0100

    login ok

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,7 +1,16 @@
 <div class="m-3">
+    @if (Route::has('login'))
     <div class="container mx-auto bg-indigo-900 text-white flex justify-around items-center rounded-sm">
-        <p class="ml-3">Login</p>
-        <p class="ml-2">Register</p>
+        @auth
+        <a href="{{ url('/dashboard') }}" class="ml-3">Tablero</a>
+        @else
+        <a href="{{ route('login') }}" class="ml-2">Iniciar</a>
+
+            @if (Route::has('register'))
+                <a href="{{ route('register') }}" class="ml-2">Registrar</a>
+            @endif
+        @endauth
+        @endif
         <div class="container flex justify-end">
             <img src="{{asset('images/insta.svg')}}" alt="Instagram" class="mt-2 ">
             <img src="{{asset('images/Twitter.svg')}}" alt="Twitter" class="mt-2 mx-1">
