ad4b7b47c389e17b07c72bb0cfa28edb243b2083
loredanapatricia <loredana.prb@gmail.com>
2021-01-28 11:04:44 +0100

    view welcome in progress

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -1,10 +1,10 @@
 <header class="flex justify-center items-center">
     <div>
-        <div>
+        <!-- <div>
             <h1 class="text-2xl font-medium mt-7">BIENVENIDOS</h1>
-        </div>
+        </div> -->
         <div>
-            <img src="{{asset('images/LogoConSentido.png')}}" alt="Logo de ConSentido" class="w-32 h-24 ml-3 mt-3">
+            <img src="{{asset('images/LogoConSentido. png')}}" alt="Logo de ConSentido" class="w-32 h-24 ml-3 mt-3">
         </div>       
     </div>  
 </header>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,26 +5,20 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="{{ mix('css/app.css') }}">
     <link rel="stylesheet" href="{!! asset('css/category.css') !!}">
-    <title>Bienvenidos</title>
+    <title>Bienvenido a ConSentido, la plataforma de venta de productos para personas con discapacidades visuales, esperamos la disfrutes.</title>
 </head>
 
 <x-header/>
 
-<body>
-    <section class="py-5 px-5 ">
-        <div>
-            <a href="#" class=""><img class="bg-indigo-900 px-5 py-1 text-white rounded-sm" src="{{asset('images/NOSOTROS.svg')}}" alt="Plataforma de venta de productos para personas con discapacidades visuales, esperamos la disfrutes."></a>
-        </div>
-    </section>
+<body>    
     <section class="py-5 px-5 ">
         <div>
             <a href="#" class="bg-indigo-900 px-7 py-1 text-white rounded-sm">CARRITO</a>
         </div>
     </section>
 
-              
+    <!-- Creo que aquí va un foreach -->
     <x-category/>
-
 
     <footer>
         <div></div>
