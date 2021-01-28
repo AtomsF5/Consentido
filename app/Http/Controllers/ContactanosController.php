a69321bb55a655fef43f58aefce01d39629733e4
J-ordiEs <70259721+J-ordiEs@users.noreply.github.com>
2021-01-28 13:34:45 +0100

    changes

--- app/Http/Controllers/ContactanosController.php
+++ app/Http/Controllers/ContactanosController.php
@@ -10,7 +10,7 @@ use Illuminate\Support\Facades\Mail;
 class ContactanosController extends Controller
 {
     public function index(){
-
+        return view(('contactanos.index'));
     }
 
     public function store(){
