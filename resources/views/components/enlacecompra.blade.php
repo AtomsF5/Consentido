79133be81c15ec2c4391b20746fd8646bc8e0ab0
Mercedes <mvazquezsan@gmail.com>
2021-01-29 00:36:53 +0100

    text in anchor shopping cart reduced in html, width and height from #cart muted

--- resources/views/components/enlacecompra.blade.php
+++ resources/views/components/enlacecompra.blade.php
@@ -1,5 +1,5 @@
 <section class="shoppingcart">
         <div>
-            <a href="#" id="cart">Carrito de la compra</a>
+            <a href="#" id="cart">Carrito de compra</a>
         </div>
 </section>
