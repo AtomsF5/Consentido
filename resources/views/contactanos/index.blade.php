11af5ab6a28f5022afc911d46b1d1b5566522947
J-ordiEs <jorge.tm.ink@outlook.com>
2021-01-28 15:58:28 +0100

    contact-us

---
+++ resources/views/contactanos/index.blade.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="stylesheet" href="{{ mix('css/app.css') }}">
+    <link rel="stylesheet" href="style.css" />
+    <title>Contactanos</title>
+</head>
+<body>
+    <header class="flex flex-col justify-center items-center">
+        <h1 class="text-2xl font-medium mt-7 uppercase">Contáctanos</h1>
+        <img src="{{asset('images/LogoConSentido.png')}}" alt="Logo de ConSentido" class="w-32 h-24 ml-3 mt-3">
+    </header>
+    <section class="p-5 flex flex-col h-screen justify-between">
+        <p>Queremos mejorar su experiencia de navegación en nuestra página.</p>
+        <p>Haznos saber tu requerimiento o sugerencia. Escríbenos.</p>
+        <form>
+            <label for="contacto"></label>
+
+            <textarea id="contacto" name="contacto" rows="5" cols="27"
+                maxlength="500"></textarea>
+
+        <P>Déjanos tu número de teléfono:</P>
+        <label for="country_code"></label>
+        <input type="tel" id="country_code" name="country_code" placeholder="+34" size="1" pattern="[A-Za-z]{3}"
+            title="Código país" alt="Código país"><br>
+        <label for="telefono"></label>
+        <input type="tel" id="phone" name="phone" placeholder="123 456 789" size="7" alt="insertar número de teléfono"
+            pattern="[0-9]{3}-[0-9]{2}-[0-9]{3}" required><br>
+        <input type="submit" class="enviar">
+        </form>
+        <option value="211"></option>
+    </section>
+    <footer>
+        <x-footer/>
+    </footer>
+    </body>
+</html>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,51 +6,22 @@
     <link rel="stylesheet" href="{{ mix('css/app.css') }}">
     <title>Bienvenidos</title>
 </head>
-<header class="flex justify-center items-center">
-    <div>
-        <div >
-            <h1 class="text-2xl font-medium mt-7">BIENVENIDOS</h1>
-        </div>
-        <div>
-            <img src="{{asset('images/LogoConSentido.png')}}" alt="Logo de ConSentido" class="w-32 h-24 ml-3 mt-3">
-        </div>
-    </div>
-</header>
 <body>
-    <section class="py-5 px-5 flex flex-col h-screen justify-between">
+    <header class="flex flex-col justify-center items-center">
+        <h1 class="text-2xl font-medium mt-7 uppercase">Bienvenidos</h1>
+        <img src="{{asset('images/LogoConSentido.png')}}" alt="Logo de ConSentido" class="w-32 h-24 ml-3 mt-3">
+    </header>
+    <section class="p-5 flex flex-col h-screen justify-between">
         <div>
             <a href="#" class=""><img class="bg-indigo-900 px-5 py-1 text-white rounded-sm" src="{{asset('images/NOSOTROS.svg')}}" alt="Plataforma de venta de productos para personas con discapacidades visuales, esperamos la disfrutes."></a>
         </div>
         <div>
             <a href="#" class="bg-indigo-900 px-7 py-1 text-white rounded-sm">CARRITO</a>
         </div>
-
-        <div>
-            <img src="{{asset('images/')}}" alt="">
-            <p>Categoria 1</p>
-
-            <div>
-
-                <div>
-                    <img src="https://www.magnicenter.com/wp-content/uploads/2017/11/lupa-convertible_03_1.jpg" alt="Gafas lectura plegable modelo F5">
-
-                </div>
-                <span>
-                    <span>
-                        <bdi>58.00&nbsp;
-                            <span class="woocommerce-Price-currencySymbol">€</span>
-                        </bdi>
-                    </span>
-                    <small class="woocommerce-price-suffix">IVA Incluido</small>
-                </span>
-            </div>
-        </div>
     </section>
-
     <section class="flex justify-end mx-3 uppercase mb-0">
         <a href="{{route('contactanos.index')}}">Contáctanos</a>
     </section>
-
     <footer>
         <x-footer/>
     </footer>
