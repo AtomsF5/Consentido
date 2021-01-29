8476f36421bee862a5c2c7fc95b99aa5ac192c11
J-ordiEs <jorge.tm.ink@outlook.com>
2021-01-29 11:34:50 +0100

    merge category

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,14 +6,12 @@
     <link rel="stylesheet" href="{{ mix('css/app.css') }}">
     <title>Bienvenidos</title>
 </head>
-<body>
+<body class="flex flex-col h-screen justify-between">
     <header class="flex flex-col justify-center items-center">
         <h1 class="text-2xl font-medium mt-7 uppercase">Bienvenidos</h1>
         <img src="{{asset('images/LogoConSentido.png')}}" alt="Logo de ConSentido" class="w-32 h-24 ml-3 mt-3">
     </header>
-    <section class="p-5 flex flex-col h-screen justify-between">
-            <a href="#" class=""><img class="bg-indigo-900 px-5 py-1 text-white rounded-sm" src="{{asset('images/NOSOTROS.svg')}}" alt="Plataforma de venta de productos para personas con discapacidades visuales, esperamos la disfrutes."></a>
-            <a href="#" class="bg-indigo-900 px-7 py-1 text-white rounded-sm">CARRITO</a>
+    <section>
     </section>
     <section class="flex justify-end mx-3 uppercase mb-0">
         <a href="{{route('contactanos.index')}}">Contáctanos</a>
