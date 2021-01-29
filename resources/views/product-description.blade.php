acde0388080eca069729d40ce6a4491081888891
Mercedes <mvazquezsan@gmail.com>
2021-01-29 13:00:37 +0100

    text in description blade modified

--- resources/views/product-description.blade.php
+++ resources/views/product-description.blade.php
@@ -21,7 +21,7 @@
     <section class="detailedproductimage">
 
     <div>
-        <img id="productimage" src="https://www.magnicenter.com/wp-content/uploads/2020/05/montana-gafas-de-lectura-720x491.jpg" alt="Gafas lectura peglable modelo F5">
+        <img id="productimage" src="https://www.magnicenter.com/wp-content/uploads/2020/05/montana-gafas-de-lectura-720x491.jpg" alt="Gafas lectura pleglable modelo F5">
     </div>
 
     </section>
@@ -31,7 +31,7 @@
     <h3>Lista detallada de características del producto</h3>
 
 <div role="list">
-    <div role="listitem"><span aria-label="1"></span> Tamaño de la lente – 58 milímetros x 35 milímetros x 132 milímetros</div>
+    <div role="listitem"><span aria-label="1"></span> Tamaño de la lente – 58 milímetros por 35 milímetros por 132 milímetros</div>
     <div role="listitem"><span aria-label="2"></span> Dispositivo de enganche recubierto en goma para evitar arañazos a sus gafas.</div>
     <div role="listitem"><span aria-label="3"></span> El marco es plegable y de quita y pon rápido.</div>
     <div role="listitem"><span aria-label="4"></span> Lente en material CR-39 orgánico.</div>
