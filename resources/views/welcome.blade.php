0e17aef8060c1f79768434703348c3aa13309fd5
Mercedes <mvazquezsan@gmail.com>
2021-01-31 00:26:06 +0100

    change in order of link enlacecompra nad category, link  anadircompra removed

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,9 +4,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="{{ mix('css/app.css') }}">
-    <link rel="stylesheet" href="{!! asset('css/category.css') !!}">
     <link rel="stylesheet" href="{!! asset('css/enlacecompra.css') !!}">
-    <link rel="stylesheet" href="{!! asset('css/anadircompra.css') !!}">
+    <link rel="stylesheet" href="{!! asset('css/category.css') !!}">
     <title>Bienvenido a ConSentido, la plataforma de venta de productos para personas con discapacidades visuales, esperamos la disfrutes.</title>
 </head>
 <body class="flex flex-col h-screen justify-between">
