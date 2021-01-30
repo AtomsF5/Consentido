76ef54a69143e56a3940e379799f4ba4da17a1ad
Mercedes <mvazquezsan@gmail.com>
2021-01-31 00:39:38 +0100

    link to contáctanos added before footer

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,6 +6,7 @@
     <link rel="stylesheet" href="{{ mix('css/app.css') }}">
     <link rel="stylesheet" href="{!! asset('css/enlacecompra.css') !!}">
     <link rel="stylesheet" href="{!! asset('css/category.css') !!}">
+    
     <title>Bienvenido a ConSentido, la plataforma de venta de productos para personas con discapacidades visuales, esperamos la disfrutes.</title>
 </head>
 <body class="flex flex-col h-screen justify-between">
