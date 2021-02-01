49ff54f44e8dd3d25377896cdc2e7e157bbedae7
Olga <o.jpadilla73@gmail.com>
2021-02-01 14:14:02 +0100

    Validation Form Request

---
+++ app/Http/Requests/ValidarFormularioRequest.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class ValidarFormularioRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return false;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'mensaje.required'   => 'El mensaje es obligatorio.',
+            'mensaje.min'        => 'El mensaje debe contener min 30 letra.',
+            'mensaje.max'        => 'El mensaje debe contener max 500 letras.',
+
+            'telefono.required'   => 'El telefono es obligatorio.',
+            'telefono'        => 'El telefono debe contener 9 digitos.',
+        ];
+    }
+}
