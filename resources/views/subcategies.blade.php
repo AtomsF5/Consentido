19a597c0e785d9e88435c02734ac6f5bf5cd521e
Mercedes <mvazquezsan@gmail.com>
2021-02-02 10:39:21 +0100

    subcategories blade renamed, subcategory css created, subcategory alt text adapted to subcategory, subcategory ling added to subcategories blade

--- resources/views/subcategies.blade.php
+++ resources/views/subcategies.blade.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="{{ mix('css/app.css') }}">
     <link rel="stylesheet" href="{!! asset('css/enlacecompra.css') !!}">
-    <link rel="stylesheet" href="{!! asset('css/category.css') !!}">
+    <link rel="stylesheet" href="{!! asset('css/subcategory.css') !!}">
     <title>Página de Subcategorías</title>
 </head>
     <body class="flex flex-col h-screen justify-between">
@@ -14,7 +14,7 @@
     
         <x-enlacecompra/>
     
-        <x-category/>
+        <x-subcategory/>
     
         <section class="flex justify-end mx-3 uppercase mb-0">
             <a href="{{route('contactanos.index')}}" class="{{request()->routeIs('contactanos.index') ? 'active' : ''}}">Contáctanos</a>
