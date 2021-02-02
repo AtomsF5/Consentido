6b7fb848fe0c2e99260f91be996d58063719b4fd
Mercedes <mvazquezsan@gmail.com>
2021-02-02 10:27:59 +0100

    subcategories blade adapted with images

--- resources/views/subcategies.blade.php
+++ resources/views/subcategies.blade.php
@@ -4,48 +4,22 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="{{ mix('css/app.css') }}">
-    <title>Bienvenidos</title>
+    <link rel="stylesheet" href="{!! asset('css/enlacecompra.css') !!}">
+    <link rel="stylesheet" href="{!! asset('css/category.css') !!}">
+    <title>Página de Subcategorías</title>
 </head>
-<header class="flex justify-center items-center">
-    <div>
-        <div >
-            <h1 class="text-2xl font-medium mt-7">SUBCATEGORIAS</h1>
-        </div>
-        <div>
-            <img src="{{asset('images/LogoConSentido.png')}}" alt="Logo de ConSentido" class="w-32 h-24 ml-3 mt-3">
-        </div>
-    </div>
-</header>
-<body>
-    <section class="py-5 px-5">
-        
-        <div>
-            <a href="#" class="bg-indigo-900 px-7 py-1 text-white rounded-sm">CARRITO</a>
-        </div>
+    <body class="flex flex-col h-screen justify-between">
 
-        <div>
-            <img src="{{asset('images/')}}" alt="">
-            <p>Subcategoria 1</p>
-
-            <div>
-               
-                <div>
-                    <img src="https://www.magnicenter.com/wp-content/uploads/2017/11/lupa-convertible_03_1.jpg" alt="Gafas lectura plegable modelo F5">
-                </div>
-                <span>
-                    <span>
-                        <bdi>58.00&nbsp;
-                            <span class="woocommerce-Price-currencySymbol">€</span>
-                        </bdi>
-                    </span>
-                    <small class="woocommerce-price-suffix">IVA Incluido</small>
-                </span>
-            </div>
-        </div>
-    </section>
-
-    <footer>
-        <div></div>
-    </footer>
+        <x-header/>
+    
+        <x-enlacecompra/>
+    
+        <x-category/>
+    
+        <section class="flex justify-end mx-3 uppercase mb-0">
+            <a href="{{route('contactanos.index')}}" class="{{request()->routeIs('contactanos.index') ? 'active' : ''}}">Contáctanos</a>
+        </section>
+    
+        <x-footer/>
 </body>
 </html>
