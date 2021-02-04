bb5ccf3dd8b600b672121d3bda85a327e0f992bf
Mercedes <mvazquezsan@gmail.com>
2021-02-04 16:47:00 +0100

    subcategory factory definition included, subcategory seeder created

--- app/Models/Subcategory.php
+++ app/Models/Subcategory.php
@@ -8,4 +8,11 @@ use Illuminate\Database\Eloquent\Model;
 class Subcategory extends Model
 {
     use HasFactory;
+
+    protected $guarded = [];
+
+    
+    protected $fillable= [
+        'title', 'urlimage', 'visit', 'order',
+    ];
 }
